182be1b70bd78ecdc190e9daeee7a0c3926eda0e
Lucas Livchits <livchitsl@gmail.com>
2019-05-11 21:52:36 -0300

    Agrega url al ejercicio en CodeWars

--- sumOfPositive.js
+++ sumOfPositive.js
@@ -2,7 +2,10 @@
 
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
-Note: if there is nothing to sum, the sum is default to 0.*/
+Note: if there is nothing to sum, the sum is default to 0.
+
+https://www.codewars.com/kata/5715eaedb436cf5606000381
+*/
 
 function positiveSum(arr) {
   let suma = 0;
